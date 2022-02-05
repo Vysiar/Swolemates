e0be207e9559b0f82f1b4d09a604d0b58992061c
Vysiar <tmarcus@vt.edu>
2022-02-05 17:10:38 -0500

    Renamed some files and finished up the welcome page

--- app/src/main/java/com/example/swolemates/WelcomePage.kt
+++ app/src/main/java/com/example/swolemates/WelcomePage.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Login_Signup.newInstance] factory method to
+ * Use the [WelcomePage.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Login_Signup : Fragment() {
+class WelcomePage : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class Login_Signup : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login__signup, container, false)
+        return inflater.inflate(R.layout.fragment_welcome_page, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class Login_Signup : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment Login_Signup.
+         * @return A new instance of fragment WelcomePage.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Login_Signup().apply {
+            WelcomePage().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
