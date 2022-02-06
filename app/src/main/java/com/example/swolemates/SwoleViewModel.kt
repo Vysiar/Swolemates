9fc9b0cd5bfc99654b34d2ab156ef12d7cdb624b
nicolefitz <nicolef00@vt.edu>
2022-02-06 11:03:15 -0500

    adding more data

--- app/src/main/java/com/example/swolemates/SwoleViewModel.kt
+++ app/src/main/java/com/example/swolemates/SwoleViewModel.kt
@@ -7,9 +7,13 @@ class SwoleViewModel(application: Application): AndroidViewModel(application) {
     var user: User = User(1, "", 0, "", Gender.OTHER, true, true)
     var otherUsers: MutableList<User> = mutableListOf(
         User(111111, "Donald Pump", 24, "Blacksburg", Gender.MALE, true, false),
-        User(222222, "Ben Bulkenson", 21, "Blacksburg", Gender.MALE, liked = false, matched = false),
         User(333333, "Debby Deadlifts", 24, "Blacksburg", Gender.FEMALE, true, false),
         User(444444, "Felicity Flash", 23, "Blacksburg", Gender.FEMALE,true, false),
-        User(555555, "Lady Macbuff", 20, "Blacksburg", Gender.FEMALE, false, false)
+        User(555555, "Lady Macbuff", 20, "Blacksburg", Gender.FEMALE, false, false),
+        User(666666, "Marcus Max", 21, "Blacksburg", Gender.MALE, true, false),
+        User(777777, "Nicole Nutrition", 21, "Blacksburg", Gender.FEMALE, false, false),
+        User(888888, "Alisa Arm Circles", 22, "Blacksburg", Gender.FEMALE, true, false),
+        User(999999, "Fast Franco", 20, "Blacksburg", Gender.MALE, true, false)
+
     )
 }
